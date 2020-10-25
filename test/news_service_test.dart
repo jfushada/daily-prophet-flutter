7c2265c8a635b9ab067954e0099201e4d5af0b77
jfushada <jessicafushada@gmail.com>
2020-10-25 16:30:43 +0700

    chore: add documentation for news_service_test.dart

--- test/news_service_test.dart
+++ test/news_service_test.dart
@@ -5,6 +5,23 @@ import 'package:test/test.dart';
 import 'package:daily_prophet_flutter/features/news/response_models/news_response.dart';
 import 'package:daily_prophet_flutter/features/news/services/news_service.dart';
 
+/// Generally you want unit tests to be fast and tightly scoped, and kind of simple in that you have an input, and you want to get a particular output. 
+/// One thing that you want to stay away from in a unit test is that you don’t want anything that relies on external resources, that isn’t part of your code.
+ 
+/// In this test, if you just actually made a http call, the test could fail anytime in the future, if you’re not connected, for example. 
+/// Try to isolate things outside of your control, and just focus on what you do with it. 
+
+/// So instead of making an actual http call, here I’m mocking it by using a package called mockito - a mock library for dart inspired by the Java version. 
+/// The function I'm mocking is in the NewsService, called topNewsList, and for testing purposes, I’m sending the http client as a parameter, so I can mock it in the test. 
+/// The MockClient class implements the http.Client class. This allows you to pass the MockClient to the topNewsList function, and return different http responses in each test. 
+/// The function basically has two results - if it succeeds, it returns the News Response, which is a model based on NewsApi, and if it fails, it throws an exception. 
+
+/// The conditions are tested using the when function - it’s using the mock client to get the responses, and then returns a successful response, which we expect to be of type NewsResponse. 
+/// In the failure case, we feed it with an error, with status code 404, and we expect that it throws an exception.
+
+/// You can also see here that you can group tests - the ones that are related. 
+/// These tests will run in order, and if one fails, you’ll see that the group name precedes the test name, so you can find the exact one you’re looking at so you’re not confused.
+
 class MockClient extends Mock implements http.Client {}
 
 main() {
